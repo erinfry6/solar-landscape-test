16a18f0388dfe3b624889842b5d6d8918962bfb5
Erin Fry <erinfry6@gmail.com>
2025-05-07 22:44:55 -0500

    Write to sqlite database

--- scripts/process.py
+++ scripts/process.py
@@ -27,12 +27,15 @@ class ProjectInfo:
             return project_info["Project Status"] == "ACTIVE" and project_info["Community Solar Status"] == "ACTIVE"    
         return False
     
+    # TODO should we validate that a project is active and provide descriptive error messages? 
+    
 class CommunitySolar:
     def __init__(self, config_path: str = "config/project/BGE Community Solar Pilot Program.yaml"):
         with open(config_path, 'r') as file:
             self.config = yaml.safe_load(file)
 
         self.project_id = self.config['project_id']
+        self.project_name = self.config['project']
         self.metadata_config = self.config['metadata']
         self.data_config = self.config['energy_data']
 
@@ -43,12 +46,23 @@ class CommunitySolar:
         metadata_cells = self.metadata_config['values']
         return read_excel_cells(self.metadata_config['path'], self.metadata_config['sheet'], metadata_cells)
 
-    def process_metadata(self):
+    def process_metadata(self) -> dict:
+        print(f"Processing metadata for project {self.project_name}...")
         metadata = self.read_metadata()
 
         # append project info to metadata
         project_info = ProjectInfo().get_project_info(self.project_id)
+
+        # rename project_info keys
+        rename_keys = {
+            "Project ID Number": "project_id",
+            "Project Status": "project_status",
+            "Community Solar Status": "community_solar_status",
+            "Maximum Monthyl kWh Production": "maximum_kwh_production",
+        }
+        project_info = {rename_keys.get(k, k): v for k, v in project_info.items()}
         metadata.update(project_info)
+        metadata['project_name'] = self.project_name
         return metadata
 
     
@@ -57,19 +71,21 @@ class CommunitySolar:
         Load energy data from the specified Excel file and sheet.
         """
         energy_data = pd.read_excel(self.data_config['path'], sheet_name=self.data_config['sheet'], header=self.data_config['header_row'])
-        rename_columns = self.data_config['column_mapping']
-        if rename_columns:
-            energy_data.rename(columns=rename_columns, inplace=True)
+        select_columns = self.data_config['column_mapping']
+
+        # return only selected columns and rename them
+        energy_data = energy_data[select_columns.keys()]
+        rename_columns = {col: select_columns[col] for col in energy_data.columns if col in select_columns}
+        energy_data = energy_data.rename(columns=rename_columns)
         
         return energy_data
     
-    def process_energy_data(self) -> pd.DataFrame:
+    def process_energy_data(self, project_id: int, report_run_date: str) -> pd.DataFrame:
         energy_data = self.get_energy_data()
         trimmed_data = trim_dataframe_at_string(energy_data)
         
+        trimmed_data['project_id'] = project_id
+        trimmed_data['report_run_date'] = report_run_date
+        
         ## TODO quality checks, flags for na?
         return trimmed_data
-
-if __name__ == "__main__":
-    solar_data = CommunitySolar()
-    print(solar_data.process_energy_data())
---
+++ scripts/write.py
@@ -0,0 +1,82 @@
+import sqlite3
+import pandas as pd
+
+
+class Database:
+    def __init__(self, db_path: str):
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
+
+    def create_tables(self):
+        # Create tables if they don't exist
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS ProjectInfo (
+            project_id INTEGER PRIMARY KEY,
+            project_name TEXT NOT NULL,
+            project_status TEXT,
+            community_solar_status TEXT,
+            maximum_kwh_production DECIMAL,
+            report_run_date TEXT NOT NULL,
+            period_start_date TEXT,
+            period_end_date TEXT,
+            account_id INTEGER,
+            account_name TEXT,
+            electric_choice_id INTEGER,
+            bge_csegs_pilot_project_id INTEGER,
+            notes TEXT,
+            some_other_field TEXT
+        )
+        ''')
+
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS EnergyData (
+            project_id INTEGER NOT NULL,
+            report_run_date TEXT NOT NULL,
+            subscriber_choice_id DECIMAL,
+            multiple_cseg_allocations TEXT,
+            allocation_percentage DECIMAL,
+            actual_kwh_allocated DECIMAL,
+            initial_balance_kwh DECIMAL,
+            kwh_allocated_from_all_subscriptions DECIMAL,
+            subscriber_billed_usage DECIMAL,
+            adjustment_kwh DECIMAL,
+            final_balance_kwh DECIMAL,
+            community_solar_adjustment DECIMAL,
+            tou_usage_on_peak_kwh DECIMAL,
+            tou_usage_intermediate_peak_kwh DECIMAL,
+            tou_usage_off_peak_kwh DECIMAL,
+            FOREIGN KEY (project_id) REFERENCES ProjectInfo (project_id),
+            FOREIGN KEY (report_run_date) REFERENCES ProjectInfo (report_run_date)
+        )
+        ''')
+
+    def insert_project_info(self, project_info: dict):
+        
+        df = pd.DataFrame([project_info])
+        try:
+            df.to_sql('ProjectInfo', self.conn, if_exists='append', index=False, method='multi')
+        except sqlite3.IntegrityError as e:
+            raise ValueError(f"Error inserting project info - check required metadata fields: {e}")
+
+
+    def insert_energy_data(self, energy_data: pd.DataFrame):
+        # Ensure the DataFrame has the minimal required columns
+        required_columns = [
+            'project_id', 'report_run_date', 'subscriber_choice_id', 
+            'allocation_percentage', 'actual_kwh_allocated', 
+            'kwh_allocated_from_all_subscriptions', 'subscriber_billed_usage',
+            'adjustment_kwh', 'final_balance_kwh', 'community_solar_adjustment'
+        ]
+        
+        if not all(col in energy_data.columns for col in required_columns):
+            raise ValueError(f"Energy data is missing required column: {', '.join([col for col in required_columns if col not in energy_data.columns])}")
+
+        try:
+            energy_data.to_sql('EnergyData', self.conn, if_exists='append', index=False, method='multi')
+        except sqlite3.IntegrityError as e:
+            raise ValueError(f"Error inserting project info - check required energy data fields: {e}")
+
+
+    def close(self):
+        self.conn.commit()
+        self.conn.close()
